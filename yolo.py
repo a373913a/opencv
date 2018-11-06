e24546653722df00084a3ffc2bcb010948e146f9
a373913a <a373913a@yahoo.com.tw>
2018-11-06 08:33:41 +0800

    image change

--- yolo.py
+++ yolo.py
@@ -19,9 +19,9 @@ from yolo3.utils import letterbox_image
 
 class YOLO(object):
     def __init__(self):
-        self.model_path = 'model_data/logstrained_weights_final_2.h5' # model path or trained weights path
+        self.model_path = 'model_data/yolo.h5' # model path or trained weights path
         self.anchors_path = 'model_data/yolo_anchors.txt'
-        self.classes_path = 'model_data/my_class.txt'
+        self.classes_path = 'model_data/coco_classes.txt'
         self.score = 0.3
         self.iou = 0.45
         self.class_names = self._get_class()
